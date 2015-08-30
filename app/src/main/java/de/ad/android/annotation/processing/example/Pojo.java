d46c81dc04d20802a477dc221759835340d701d7
a11n <andre.diermann@gmail.com>
2015-08-30 15:37:29 +0200

    Fixed project configuration in order to use DataProcessor for annotation processing.

---
+++ app/src/main/java/de/ad/android/annotation/processing/example/Pojo.java
@@ -0,0 +1,6 @@
+package de.ad.android.annotation.processing.example;
+
+import de.ad.annotation.processing.api.Data;
+
+@Data public class Pojo {
+}
--- compiler/src/main/java/de/ad/annotation/processing/compiler/DataProcessor.java
+++ compiler/src/main/java/de/ad/annotation/processing/compiler/DataProcessor.java
@@ -1,13 +1,38 @@
 package de.ad.annotation.processing.compiler;
 
+import de.ad.annotation.processing.api.Data;
+import java.util.Collections;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 
-public class DataProcessor extends AbstractProcessor{
+public class DataProcessor extends AbstractProcessor {
+
+  private Messager messenger;
+
+  @Override public synchronized void init(ProcessingEnvironment processingEnv) {
+    super.init(processingEnv);
+
+    messenger = processingEnv.getMessager();
+  }
+
+  @Override public Set<String> getSupportedAnnotationTypes() {
+    return Collections.singleton(Data.class.getCanonicalName());
+  }
+
+  @Override public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();
+  }
+
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    return false;
+    messenger.printMessage(Diagnostic.Kind.NOTE, "sdfsfsdfsdfs");
+
+    return true;
   }
 }
